07f2130934e9753a44ed245b0b1ca8bd483b9934
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-10 16:13:33 -0500

    add/ Test and code summaryFrom

---
+++ src/pages/summary/tests/SummaryForm.test.js
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react"; // render the components, screen allows us find elements and fireEvent simulated user interactive
+import { SummaryForm } from "../SummaryForm";
+
+describe("Testing <SummaryForm />", () => {
+  test("1. Initial conditions", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    expect(checkbox).not.toBeChecked();
+
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+    expect(confirmButton).toBeDisabled();
+  });
+
+  test("2. Checkbox enables button on first click and disables on second click", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
+
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeDisabled();
+  });
+});
